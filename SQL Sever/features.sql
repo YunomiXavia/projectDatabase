210388bd9db8b24adae528092fa4fd7ba4ec3ab2
Sukmadi <nhatthanhdinh482@gmail.com>
2024-05-03 21:00:38 +0700

    insert and update

--- SQL Sever/features.sql
+++ SQL Sever/features.sql
@@ -12,6 +12,32 @@ use pm;
 --     ('Project E', 'Internet of Things (IoT) Solution Development', 0, '["Embedded Systems", "Cloud Computing", "Data Engineering"]', 'High', 'In Progress', '2023-05-01', NULL, 6);
 
 
-select * from Project
 
+-- INSERT INTO Role (role_name, role_description)
+-- VALUES 
+--     ('Developer', 'Responsible for writing code and implementing software solutions.'),
+--     ('Tester', 'Responsible for testing software applications to ensure quality and functionality.'),
+--     ('Project Manager', 'Responsible for overseeing project execution and ensuring its success.'),
+--     ('Database Administrator', 'Responsible for managing and maintaining databases.'),
+--     ('UI/UX Designer', 'Responsible for designing user interfaces and experiences.'),
+--     ('Data Scientist', 'Responsible for analyzing and interpreting complex data sets to inform business decisions.');
+--     ('Web Development', 'Development of websites and web applications using various technologies'),
+--     ('Testing', 'Conducting tests to ensure the quality and functionality of software applications'),
+--     ('Marketing', 'Promoting products or services to attract customers');
+--     select * from Role
 
+-- -- Now, let's insert some teams along with their skills and team leads
+-- INSERT INTO Team (team_name, team_skill_id, team_lead_id)
+-- VALUES 
+--     ('Web Development Team', (SELECT skill_id FROM Skills WHERE skill_name = 'Web Development'), (SELECT employee_id FROM Employee WHERE first_name = 'Michael' AND last_name = 'Johnson')),
+--     ('Testing Team', (SELECT skill_id FROM Skills WHERE skill_name = 'Testing'), (SELECT employee_id FROM Employee WHERE first_name = 'Emily' AND last_name = 'Davis')),
+--     ('Marketing Team', (SELECT skill_id FROM Skills WHERE skill_name = 'Marketing'), (SELECT employee_id FROM Employee WHERE first_name = 'David' AND last_name = 'Wilson'));
+
+-- select * from Skills
+-- Now, let's insert some teams along with their skills (without team leads)
+-- INSERT INTO Team (team_name, team_skill_id)
+-- VALUES 
+--     ('Backend Team', (SELECT skill_id FROM Skills WHERE skill_name = 'Backend Development')),
+--     ('Design Team', (SELECT skill_id FROM Skills WHERE skill_name = 'UI/UX Design')),
+--     ('Data Analysis Team', (SELECT skill_id FROM Skills WHERE skill_name = 'Data Science'));
+select * from team_member
