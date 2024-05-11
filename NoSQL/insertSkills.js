33d916be879254e7c177d9e6403f35137c59ba9c
Yunomi Xavia <yunomix2834@gmail.com>
2024-05-11 21:44:25 +0700

    create Mongodb script

---
+++ NoSQL/insertSkills.js
@@ -0,0 +1,427 @@
+db.Skills.insertMany([
+  {
+    _id: ObjectId(),
+    skill_id: 1,
+    skill_name: "Web Development",
+    skill_description:
+      "Development of websites and web applications using various technologies",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 2,
+    skill_name: "Tester",
+    skill_description:
+      "Testing software applications to ensure quality and functionality",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 3,
+    skill_name: "Marketing",
+    skill_description: "Promoting products or services to attract customers",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 4,
+    skill_name: "Artificial Intelligence",
+    skill_description:
+      "Development of systems that simulate human intelligence",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 5,
+    skill_name: "Machine Learning",
+    skill_description:
+      "Creating algorithms that improve automatically through experience",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 6,
+    skill_name: "Software Engineering",
+    skill_description:
+      "Designing and building software systems efficiently and reliably",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 7,
+    skill_name: "Mobile Development",
+    skill_description:
+      "Creating applications for mobile devices such as smartphones and tablets",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 8,
+    skill_name: "QA",
+    skill_description: "Testing and ensuring the quality of software products",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 9,
+    skill_name: "Backend Development",
+    skill_description:
+      "Building server-side logic and databases for web applications",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 10,
+    skill_name: "Database Administration",
+    skill_description:
+      "Managing and maintaining databases to ensure performance and security",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 11,
+    skill_name: "UI/UX Design",
+    skill_description:
+      "Designing user interfaces and experiences for digital products",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 12,
+    skill_name: "Data Science",
+    skill_description:
+      "Extracting insights and knowledge from data through various techniques",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 13,
+    skill_name: "Business Analysis",
+    skill_description:
+      "Analyzing business processes and identifying opportunities for improvement",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 14,
+    skill_name: "Embedded Systems",
+    skill_description:
+      "Designing and implementing systems with embedded hardware and software",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 15,
+    skill_name: "Cloud Computing",
+    skill_description:
+      "Using remote servers hosted on the internet to store, manage, and process data",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 16,
+    skill_name: "Data Engineering",
+    skill_description:
+      "Designing and managing large-scale data processing systems",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 17,
+    skill_name: "Python",
+    skill_description:
+      "A high-level programming language known for its simplicity and readability.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 18,
+    skill_name: "Data Analysis",
+    skill_description:
+      "The process of inspecting, cleaning, transforming, and modeling data to discover useful information, inform conclusions, and support decision-making.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 19,
+    skill_name: "JavaScript",
+    skill_description:
+      "A programming language that enables interactive web pages and dynamic content.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 20,
+    skill_name: "Database Management",
+    skill_description:
+      "The process of administering, controlling, and managing databases to ensure their security, integrity, and performance.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 21,
+    skill_name: "Java",
+    skill_description:
+      "A popular, object-oriented programming language used for developing a wide range of applications.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 22,
+    skill_name: "C++",
+    skill_description:
+      "A versatile programming language known for its efficiency and performance, commonly used in system and game development.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 23,
+    skill_name: "C#",
+    skill_description:
+      "A modern programming language developed by Microsoft, often used for building Windows applications and games.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 24,
+    skill_name: "Ruby",
+    skill_description:
+      "A dynamic, reflective, object-oriented programming language known for its simplicity and productivity.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 25,
+    skill_name: "PHP",
+    skill_description:
+      "A server-side scripting language used for web development and creating dynamic web pages.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 26,
+    skill_name: "Swift",
+    skill_description:
+      "A powerful and intuitive programming language used for developing iOS, macOS, watchOS, and tvOS apps.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 27,
+    skill_name: "Back-end Development",
+    skill_description:
+      "The development of server-side logic and databases for web applications.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 28,
+    skill_name: "Front-end Development",
+    skill_description:
+      "The development of client-side interfaces and user experiences for web applications.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 29,
+    skill_name: ".NET",
+    skill_description:
+      "A framework developed by Microsoft for building various types of applications, including web, desktop, and mobile applications.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 30,
+    skill_name: "Web API",
+    skill_description:
+      "A framework for building HTTP services that can be consumed by a broad range of clients, including browsers, mobile devices, and IoT devices.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 31,
+    skill_name: "Web MVC",
+    skill_description:
+      "A design pattern and framework for building web applications that separates the application into three main components: Model, View, and Controller.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 32,
+    skill_name: "DevOps",
+    skill_description:
+      "Combines software development and IT operations to streamline the systems development life cycle and enable continuous delivery with high software quality.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 33,
+    skill_name: "Kotlin",
+    skill_description:
+      "A statically typed programming language developed by JetBrains and officially supported by Google as an Android development language.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 34,
+    skill_name: "Embedded Systems Engineer",
+    skill_description:
+      "Responsible for designing and developing embedded systems, including hardware and firmware components.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 35,
+    skill_name: "Marketing Strategist",
+    skill_description:
+      "Develops marketing strategies and plans to promote products or services, analyzes market trends, and implements marketing campaigns.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 36,
+    skill_name: "Machine Learning Engineer",
+    skill_description:
+      "Builds and implements machine learning models and algorithms to analyze data, extract insights, and make predictions.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 37,
+    skill_name: "UX Designer",
+    skill_description:
+      "Focuses on enhancing user satisfaction by improving the usability, accessibility, and pleasure provided in the interaction between the user and the product.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 38,
+    skill_name: "Data Scientist",
+    skill_description:
+      "Uses statistical techniques, machine learning algorithms, and programming skills to analyze large datasets and extract meaningful insights.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 39,
+    skill_name: "System Administrator",
+    skill_description:
+      "Manages and maintains computer systems, networks, and software, ensuring their performance, reliability, and security.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 40,
+    skill_name: "Social Media Manager",
+    skill_description:
+      "Creates and manages content on social media platforms to engage audiences, build brand awareness, and drive traffic to websites.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 41,
+    skill_name: "Hardware Engineer",
+    skill_description:
+      "Designs, develops, and tests computer hardware components, such as processors, memory modules, and circuit boards.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 42,
+    skill_name: "Systems Engineer",
+    skill_description:
+      "Plans, designs, and manages complex systems, integrating hardware, software, and network solutions to meet specific requirements.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 43,
+    skill_name: "Back-end Developer",
+    skill_description:
+      "Builds and maintains server-side applications and databases, ensuring high performance, scalability, and security.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 44,
+    skill_name: "Cloud Engineer",
+    skill_description:
+      "Designs, implements, and manages cloud-based infrastructure and services, leveraging platforms such as AWS, Azure, or Google Cloud.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 45,
+    skill_name: "Firmware Engineer",
+    skill_description:
+      "Develops and optimizes firmware for embedded systems, including low-level programming and hardware interfacing.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 46,
+    skill_name: "Content Creator",
+    skill_description:
+      "Produces engaging and relevant content for various platforms, such as websites, blogs, social media, and marketing materials.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 47,
+    skill_name: "Front-end Developer",
+    skill_description:
+      "Builds and maintains client-side applications and user interfaces using HTML, CSS, and JavaScript frameworks.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 48,
+    skill_name: "Quality Assurance Specialist",
+    skill_description:
+      "Tests software applications to ensure they meet quality standards, identify defects, and improve overall product quality.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 49,
+    skill_name: "Automation Engineer",
+    skill_description:
+      "Designs, develops, and implements automated test scripts and frameworks to streamline testing processes and improve efficiency.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 50,
+    skill_name: "Mobile Developer",
+    skill_description:
+      "Develops mobile applications for iOS or Android platforms, using programming languages such as Swift, Kotlin, or Java.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 51,
+    skill_name: "Database Administrator",
+    skill_description:
+      "Manages and maintains databases, ensuring data integrity, security, and availability.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 52,
+    skill_name: "Graphic Designer",
+    skill_description:
+      "Creates visual concepts and designs for various purposes, including branding, marketing materials, and user interfaces.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 53,
+    skill_name: "UI/UX Designer",
+    skill_description:
+      "Focuses on enhancing user satisfaction by improving the usability, accessibility, and aesthetics of digital products.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 54,
+    skill_name: "Data Analyst",
+    skill_description:
+      "Analyzes data to identify trends, patterns, and insights that can inform business decisions and strategies.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 55,
+    skill_name: "UI Designer",
+    skill_description:
+      "Designs the user interface for digital products, including layout, navigation, and visual elements.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 56,
+    skill_name: "Project Manager",
+    skill_description:
+      "Oversees projects from initiation to completion, managing resources, timelines, and budgets to ensure successful delivery.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 57,
+    skill_name: "DevOps Engineer",
+    skill_description:
+      "Combines software development and IT operations to improve collaboration, automate processes, and deploy software more reliably and efficiently.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 58,
+    skill_name: "Write Document",
+    skill_description:
+      "Ability to effectively write and maintain technical documentation for projects.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 59,
+    skill_name: "CI/CD",
+    skill_description:
+      "Understanding and implementation of continuous integration and continuous deployment pipelines.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 60,
+    skill_name: "Git",
+    skill_description:
+      "Proficiency in using Git version control system for source code management.",
+  },
+  {
+    _id: ObjectId(),
+    skill_id: 61,
+    skill_name: "Bash",
+    skill_description:
+      "Proficiency in using Bash scripting language for automation and system administration tasks.",
+  },
+]);
---
+++ NoSQL/insertteam_skill_tag.js
@@ -0,0 +1,11 @@
+db.team_skill_tag.insertMany([
+  { team_id: 1, skill_id: 1 },
+  { team_id: 1, skill_id: 2 },
+  { team_id: 1, skill_id: 9 },
+  { team_id: 1, skill_id: 30 },
+  { team_id: 1, skill_id: 58 },
+  { team_id: 1, skill_id: 31 },
+  { team_id: 8, skill_id: 53 },
+  { team_id: 8, skill_id: 52 },
+  { team_id: 8, skill_id: 58 },
+]);
